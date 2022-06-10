24ccb8fc9a230d0c1c14e799f0dce09096dbc0b7
Emmanuel Omondi Aloo <aloo25emmanuel@gmail.com>
2022-06-10 19:03:25 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+
+ function returnFirstTwoDrivers(){
+    return ["Antonia","Nuru"]
+ }
+
+ function returnLastTwoDrivers(){
+    return ["Amari","Mo"]
+
+ }
+
+  const selectingDrivers= [ returnFirstTwoDrivers,returnLastTwoDrivers]
+
+
+  function createFareMultiplier(multiplier) {
+    
+let x = function(fare){
+    return fare*multiplier
+}
+    return x
+  }
+
+  const fareDoubler= createFareMultiplier(2)
+
+  const fareTripler= createFareMultiplier(3)
+
+  function selectDifferentDrivers(drivers,kimani){
+   return  kimani()
+
+  }
+ 
+    
+  
